fbe39abae856e87a899e1c7204ff1b122325b459
FelipeSeleme <felipeselemeribeiro@gmail.com>
2023-02-28 15:50:03 -0300

    README.md and PY file update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spreadsheets_check
-This code reads and compares the values ​​between spreadsheets in the financial and accounting sectors of a company.
+This code reads and compares the values between spreadsheets in the financial and accounting sectors of a company.
+  
+The problem was that the accounting entries needed to be checked against the financial realized in large amounts. Also, 
+the data did not have any kind of primary key. The amounts could occur in fractions, spread across different cost centers, 
+with no direct relationship between the data in the two spreadsheets. It was necessary to group accounts by sector, 
+create a key, calculate totals and differences, then return a third consolidated spreadsheet.
--- spCheck.py
+++ spCheck.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
-# configurações de exibição do pd:
+# pd display settings:
 pd.set_option('display.width', 1080)
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.float_format', '{:.2f}'.format)
 
-# leitura das planilhas a serem conciliadas:
+# reading the worksheets:
 df_financeiro = pd.read_excel(r'Conciliacao.xlsx', sheet_name="Financeiro")
 df_contabil = pd.read_excel(r'Conciliacao.xlsx', sheet_name="Contábil")
 
-# filtro das colunas a serem utilizadas:
+# columns filter:
 df_financeiro = df_financeiro[['TITULO', 'REDUZIDO', 'VAL_ATUAL']].round(2)
 df_contabil = df_contabil[['Documento', 'Reduzido', 'Valor']].round(2)
 
-# criação das chaves financeiras e contábeis (índices):
+# creation of financial and accounting keys (indexes):
 for indice in df_financeiro.index:
     df_financeiro.loc[indice, 'CHAVE'] = \
         f"{str(df_financeiro.loc[indice, 'REDUZIDO'])}/{str(df_financeiro.loc[indice, 'TITULO'])}"
@@ -21,19 +21,19 @@ for indice in df_contabil.index:
     df_contabil.loc[indice, 'CHAVE'] = \
         f"{str(df_contabil.loc[indice, 'Reduzido'])}/{str(df_contabil.loc[indice, 'Documento'])}"
 
-# agrupamento dos valores por chave:
+# grouping values by key:
 df_valor_fin = df_financeiro[['CHAVE', 'VAL_ATUAL']].groupby('CHAVE').sum()
 df_valor_con = df_contabil[['CHAVE', 'Valor']].groupby('CHAVE').sum()
 
-# mesclagem dos valores das planilhas conforme as chaves:
+# merge of the values of the worksheets according to the keys:
 df_concilia = df_valor_fin.merge(df_valor_con, how='outer', on='CHAVE')\
     .rename(columns={'VAL_ATUAL': 'Financeiro', 'Valor': 'Contabil'}).round(2)
 
-# cálculo das diferenças entre os valores totais das chaves:
+# calculation of the differences between the total values of the keys:
 for chave in df_concilia.index:
     df_concilia.loc[chave, 'Diferenca'] = df_concilia.loc[chave, 'Financeiro'] + df_concilia.loc[chave, 'Contabil']
 df_concilia = df_concilia.round(2)
 print(df_concilia)
 
-# exportação do resultado para arquivo Excel:
+# export of result to Excel file:
 df_concilia.to_excel('Resultado.xlsx')
